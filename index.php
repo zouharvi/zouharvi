f03ab1191ba542a30e8696f4663c06e5057d16a6
Vilém Zouhar <vilem.zouhar@gmail.com>
2022-05-09 13:42:49 +0200

    update pubs and styling

--- index.php
+++ index.php
@@ -8,15 +8,15 @@
   <meta name="keywords" content="Vilém Zouhar, zouharvi, programmer, portfolio, student, mathematics, TypeScript, Python, Linguistics, Research">
   <meta name="author" content="Vilém Zouhar">
   <link href="https://fonts.googleapis.com/css2?family=Mukta&display=swap" rel="stylesheet">
-  <link rel='stylesheet' type='text/css'  href='css/style.css'>
+  <link rel='stylesheet' type='text/css'  href='css/style.css?v=3'>
   <link rel='icon'       type='image/png' href='src/favicon.png'>
   <title>Vilda | Vilém Zouhar</title>
   <meta name="viewport" content="width=device-width">
 </head>
 
 <body style='padding-top: 50px; padding-bottom: 100px; background-color: #F0F0F0;'>
-  <div id='whole' style='max-width: 910px; margin:auto;'>
-    <div id='menu' style='display: inline-block; max-width: 200px; margin-right: 20px; vertical-align: top;'>
+  <div id='whole' style='max-width: 950px; margin:auto;'> 
+    <div id='menu' style='display: inline-block; margin-right: 20px; vertical-align: top;'>
       <header id='name' style='margin-left: 20px; margin-top: 20px;'>Vilém Zouhar</header>
       <img src='src/portrait.webp' alt='portrait' style='width: 120px; margin-left: 15px; margin-top: 10px;'>
       <nav style="width: 150px">
@@ -27,7 +27,7 @@
         </ul>
       </nav>
     </div>
-    <main id='container' style='display: inline-block; max-width: 700px; min-width: 300px;'>
+    <main id='container' style='display: inline-block; max-width: 760px; min-width: 300px;'>
       <div style='width: 100%'>
         <?php
             $allowed = array('about', 'projects', 'publications');
