41debe014dfcfd0d4ee8c935f6516e1235bf78ad
Vilém Zouhar <vilem.zouhar@gmail.com>
2024-03-01 11:10:49 +0100

    new paper, new json-based data source for publications

--- publications.php
+++ publications.php
@@ -1,68 +1,35 @@
 <?php
-function get_links($url_field) {
-  $out = "";
-  preg_match('/.*\|PAPER:([^|]*).*/', $url_field, $matches);
-  if (sizeof($matches) >= 2) {
-    $out .= "<a href='" . $matches[1] . "' class='paper_details_link'>paper</a>";
-  }
-  preg_match('/.*\|LINK:([^|]*).*/', $url_field, $matches);
-  if (sizeof($matches) >= 2) {
-    $out .= "<a href='" . $matches[1] . "' class='paper_details_link'>link</a>";
-  }
-  preg_match('/.*\|TOOL:([^|]*).*/', $url_field, $matches);
-  if (sizeof($matches) >= 2) {
-    $out .= "<a href='" . $matches[1] . "' class='paper_details_link'>tool</a>";
-  }
-  preg_match('/.*\|DEMO:([^|]*).*/', $url_field, $matches);
-  if (sizeof($matches) >= 2) {
-    $out .= "<a href='" . $matches[1] . "' class='paper_details_link'>demo</a>";
-  }
-  preg_match('/.*\|VIDEO:([^|]*).*/', $url_field, $matches);
-  if (sizeof($matches) >= 2) {
-    $out .= "<a href='" . $matches[1] . "' class='paper_details_link'>video</a>";
-  }
-  preg_match('/.*\|DATA:([^|]*).*/', $url_field, $matches);
-  if (sizeof($matches) >= 2) {
-    $out .= "<a href='" . $matches[1] . "' class='paper_details_link'>dataset</a>";
-  }
-  preg_match('/.*\|CODE:([^|]*).*/', $url_field, $matches);
-  if (sizeof($matches) >= 2) {
-    $out .= "<a href='" . $matches[1] . "' class='paper_details_link'>code</a>";
-  }
-  return $out;
-}
-
-function publication_entry($line, $id) {
-  // 0 - title
-  // 1 - authors | short caption
-  // 2 - venue
-  // 3 - links
-  // 4 - details
-  // 5 - img
-  if ($line[5] != "") {
-    $img = "<img src='pages/img/" . $line[5] . "'  loading='lazy'>";
+function publication_entry($item) {
+  if ($item["image"] != "") {
+    $img = "<img src='pages/img/" . $item["image"] . "'  loading='lazy'>";
     $extraclass = "paper_details_withimg";
   } else {
     $img = "";
     $extraclass = "";
   }
 
-  $authors = str_replace(" ", "&nbsp;", $line[1]);
-  $authors = str_replace(",&nbsp;", ", ", $authors);
-  $authors = str_replace(",<sup>=</sup>&nbsp;", ",<sup>=</sup> ", $authors);
+  $author = str_replace(" ", "&nbsp;", $item["author"]);
+  $author = str_replace(",&nbsp;", ", ", $author);
+  $author = str_replace(",<sup>=</sup>&nbsp;", ",<sup>=</sup> ", $author);
+
+  $links = "";
+  foreach ($item["links"] as $key => $value) {
+      $links .= "<a href='" . $value . "' class='paper_details_link'>" . $key . "</a>";
+  }
 
   return "
-    <div class='paper_title' paper_target='" . $id . "'>"
-      . $line[0] . 
-      "<span class='venue'>" . $line[2] . 
-          "<img src='src/expand_button.svg' class='paper_details_arrow' id='paper_details_arrow_" . $id . "'>" . 
+    <div class='paper_title' paper_target='" . $item["key"] . "'>"
+      . $item["title"] . 
+      "<span class='venue'>" . $item["venue"] . 
+          "<img src='src/expand_button.svg' class='paper_details_arrow' id='paper_details_arrow_" . $item["key"] . "'>" . 
       "</span>
     </div>
-    <div class='paper_details " . $extraclass . "' id='paper_details_" . $id . "'>" .
-    get_links($line[3]) . "<br><br>" .
-    $img . 
-      "<span class='authors_span'><b>" . $authors . "</b></span><br>" .
-      "<span>" . $line[4] . "</span>" .
+    <div class='paper_details " . $extraclass . "' id='paper_details_" . $item["key"] . "'>" .
+      $links .
+      "<br><br>" .
+      $img . 
+        "<span class='authors_span'><b>" . $author . "</b></span><br>" .
+        "<span>" . $item["abstract"] . "</span>" .
     "</div>
   ";
 }
@@ -76,44 +43,41 @@ function publication_entry($line, $id) {
 </h3>
 
 <?php
-    $handle = fopen("pages/publications.csv", "r");
-
-    $i = 0;
-    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-        $i += 1;
-        echo publication_entry($data, "pu" . $i);
+    $data = json_decode(file_get_contents("pages/publications.json"),TRUE);
+    foreach ($data as &$item) {
+      if ($item["type"] == "publication") {
+        echo publication_entry($item);
+      }
     }
-    fclose($handle);
 ?>
-
-
 
 <h3>Less-serious projects</h3>
 <?php
-    $handle = fopen("pages/projects.csv", "r");
-
-    $i = 0;
-    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-        $i += 1;
-        echo publication_entry($data, "pr" . $i);
+  foreach ($data as &$item) {
+    if ($item["type"] == "project") {
+      echo publication_entry($item);
     }
-    fclose($handle);
+  }
 ?>
 
 
 <script>
-let OPEN_PAPERS = [2, 5];
+let OPEN_PAPERS = [
+  "bio_mqm_metrics_bias",
+  "trust_intervention",
+  "tokenization_principle"
+];
 
-$(".paper_title").each((element_i, element) => {
-  let target_i = $(element).attr("paper_target")
+$(".paper_title").each((_, element) => {
+  let target_key = $(element).attr("paper_target")
   $(element).click(() => {
-    $("#paper_details_" + target_i).toggle();
-    $("#paper_details_arrow_" + target_i).toggleClass("paper_details_arrow_up");
+    $("#paper_details_" + target_key).toggle();
+    $("#paper_details_arrow_" + target_key).toggleClass("paper_details_arrow_up");
     $(element).toggleClass("paper_title_active");
   })
-  $("#paper_details_" + target_i).hide()
+  $("#paper_details_" + target_key).hide()
 
-  if (OPEN_PAPERS.includes(element_i)) {
+  if (OPEN_PAPERS.includes(target_key)) {
     $(element).trigger("click");
   }
 })
@@ -124,9 +88,9 @@ $("#all_button_expand").on("click", () => {
 
   // open all
   $(".paper_title").each((_, element) => {
-    let target_i = $(element).attr("paper_target")
-    $("#paper_details_" + target_i).toggle(true);
-    $("#paper_details_arrow_" + target_i).toggleClass("paper_details_arrow_up", true);
+    let target_key = $(element).attr("paper_target")
+    $("#paper_details_" + target_key).toggle(true);
+    $("#paper_details_arrow_" + target_key).toggleClass("paper_details_arrow_up", true);
     $(element).toggleClass("paper_title_active", true);
   })
 })
@@ -135,11 +99,11 @@ $("#all_button_hide").on("click", () => {
   $("#all_button_expand").toggle(true)
   $("#all_button_hide").toggle(false)
 
-  // gude all
+  // hide all
   $(".paper_title").each((_, element) => {
-    let target_i = $(element).attr("paper_target")
-    $("#paper_details_" + target_i).toggle(false);
-    $("#paper_details_arrow_" + target_i).toggleClass("paper_details_arrow_up", false);
+    let target_key = $(element).attr("paper_target")
+    $("#paper_details_" + target_key).toggle(false);
+    $("#paper_details_arrow_" + target_key).toggleClass("paper_details_arrow_up", false);
     $(element).toggleClass("paper_title_active", false);
   })
 })
