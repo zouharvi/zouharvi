9f14e3339304b99934ff582232a39682ed0cb567
Vilém Zouhar <vilem.zouhar@gmail.com>
2024-12-20 10:59:11 +0100

    update invited talks

--- index.php
+++ index.php
@@ -90,7 +90,17 @@
     <ul style="padding-left: 1em;">
     <?php
       function talk_entry($item) {
-        return "<li>" . $item["title"] . " at " . $item["venue"] . " (" . $item["date"] . ")</li>";
+        // map
+        $venues = array_map(function($venue) {
+          // if link is not empty
+          if ($venue["link"] == "") {
+            return $venue["venue"] . " (" . $venue["date"] . ")";
+          }
+          return $venue["venue"] . " (<a href='" . $venue["link"] . "'>" . $venue["date"] . "</a>)";
+        }, $item["venues"]);
+        // joint with comma
+        $venues = implode(", ", $venues);
+        return "<li>" . $item["title"] . " at " . $venues . "</li>";
       }
 
       $data = json_decode(file_get_contents("talks.json"),TRUE);
